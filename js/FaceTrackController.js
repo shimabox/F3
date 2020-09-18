1384b6033df0b29e14d6a9939a9c32701d78df74
shimabox <shimabox@gmail.com>
2020-09-18 15:45:58 +0900

    :tada: 部品コントローラのクラスに処理を委譲する

--- js/FaceTrackController.js
+++ js/FaceTrackController.js
@@ -1,47 +1,227 @@
-class FaceTrackController {
-    _faceTracker;
+'use strict';
 
+class FaceTrackController {
     constructor(faceTracker) {
         this._faceTracker = faceTracker;
-        this._setup();
+
+        this._playButton = document.querySelector('#play');
+
+        this._guiParameter = {
+            face: false,
+            leftEye: true,
+            rightEye: true,
+            nose: true,
+            mouth: true,
+            none: true,
+            mosaic: false,
+            line:false,
+            autoPlace: false
+        }
+        this._gui = new dat.GUI(this._guiParameter);
+
+        this.leftEye = new LeftEye(document.createElement('canvas'));
+        this.leftEyeBlow = new LeftEyeBlow(document.createElement('canvas'));;
+        this.rightEye = new RightEye(document.createElement('canvas'));;
+        this.rightEyeBlow = new RightEyeBlow(document.createElement('canvas'));;
+        this.nose = new Nose(document.createElement('canvas'));;
+        this.mouth = new Mouth(document.createElement('canvas'));;
+
+        this._setUp();
     }
 
     run() {
         this._faceTracker.start();
     }
 
-    _setup() {
-        this._setupDebug();
-        this._setupEyeLine();
+    _setUp() {
+        this._setUpPlayButton();
         this._setUpSwitchCamera();
+        this._setUpDebug();
+        this._setUpGuiDat();
     }
 
-    _setupDebug() {
-        const forDebug = document.querySelector('#for-debug');
-        forDebug.addEventListener('change', (e) => {
+    _setUpPlayButton() {
+        this._playButton.addEventListener('click', (e) => {
             if (e.target.checked) {
-                this.faceTracker.startDebug();
+                this._faceTracker.start();
             } else {
-                this.faceTracker.stopDebug();
+                this._faceTracker.stop();
             }
-        });
-    }
-
-    _setupEyeLine() {
-        const eyeLine = document.querySelector('#eyeLine');
-        eyeLine.addEventListener('change', (e) => {
-            if (e.target.checked) {
-                this.faceTracker.addEyeLine();
-            } else {
-                this.faceTracker.removeEyeLine();
-            }
-        });
+        })
     }
 
     _setUpSwitchCamera() {
         const switchCameraButton = document.querySelector('#switch-camera');
         switchCameraButton.addEventListener('click', (e) => {
-            this.faceTracker.switchCamera();
+            this._faceTracker.switchCamera();
+        })
+    }
+
+    _setUpDebug() {
+        const forDebug = document.querySelector('#debug');
+        forDebug.addEventListener('change', (e) => {
+            if (e.target.checked) {
+                this._faceTracker.startDebug();
+            } else {
+                this._faceTracker.stopDebug();
+            }
+        })
+    }
+
+    _setUpGuiDat() {
+        this._gui.close();
+        document.querySelector('#guiController').appendChild(this._gui.domElement);
+
+        this._setUpFaceController();
+        this._setUpFacePartsController();
+        this._setUpEyeLineController();
+    }
+
+    _setUpFaceController() {
+        const face = new Face(document.createElement('canvas'));
+        this._gui.add(this._guiParameter, 'face')
+            .name('Face Only')
+            .listen()
+            .onChange((checked) => setFaceOnly('face', checked));
+
+        const setFaceOnly = (prop, checked) => {
+            const partsParameter = [
+                'leftEye',
+                'rightEye',
+                'nose',
+                'mouth'
+            ]
+
+            this._guiParameter[prop] = checked;
+
+            if (checked) {
+                for (const param of partsParameter){
+                    this._guiParameter[param] = false;
+                    this._faceTracker.remove(param);
+                }
+                this._faceTracker.remove('leftEyeBlow');
+                this._faceTracker.remove('rightEyeBlow');
+                this._faceTracker.add('face', face);
+            } else {
+                this._faceTracker.remove('face');
+            }
+        }
+    }
+
+    _setUpFacePartsController() {
+        const facePartsFolder = this._gui.addFolder('Face Parts');
+        facePartsFolder.open();
+
+        this._setUpLeftEyePartsController(facePartsFolder);
+        this._setUpRightEyePartsController(facePartsFolder);
+        this._setUpNosePartsController(facePartsFolder);
+        this._setUpMouthEyePartsController(facePartsFolder);
+    }
+
+    _setUpLeftEyePartsController(folder) {
+        const leftEyeController = folder.add(this._guiParameter, 'leftEye').name('Left Eye');
+
+        if (leftEyeController.getValue() === true) {
+            this._faceTracker.add('leftEye', this.leftEye);
+            this._faceTracker.add('leftEyeBlow', this.leftEyeBlow);
+        }
+
+        leftEyeController.listen().onChange((checked) => {
+            if (checked) {
+                this._faceTracker.add('leftEye', this.leftEye);
+                this._faceTracker.add('leftEyeBlow', this.leftEyeBlow);
+                this._guiParameter['face'] = false;
+                this._faceTracker.remove('face');
+            } else {
+                this._faceTracker.remove('leftEye');
+                this._faceTracker.remove('leftEyeBlow');
+            }
         });
+    }
+
+    _setUpRightEyePartsController(folder) {
+        const rightEyeController = folder.add(this._guiParameter, 'rightEye').name('Right Eye');
+
+        if (rightEyeController.getValue() === true) {
+            this._faceTracker.add('rightEye', this.rightEye);
+            this._faceTracker.add('rightEyeBlow', this.rightEyeBlow);
+        }
+
+        rightEyeController.listen().onChange((checked) => {
+            if (checked) {
+                this._faceTracker.add('rightEye', this.rightEye);
+                this._faceTracker.add('rightEyeBlow', this.rightEyeBlow);
+                this._guiParameter['face'] = false;
+                this._faceTracker.remove('face');
+            } else {
+                this._faceTracker.remove('rightEye');
+                this._faceTracker.remove('rightEyeBlow');
+            }
+        });
+    }
+
+    _setUpNosePartsController(folder) {
+        const noseController = folder.add(this._guiParameter, 'nose').name('Nose');
+        if (noseController.getValue() === true) {
+            this._faceTracker.add('nose', this.nose);
+        }
+        noseController.listen().onChange((checked) => {
+            if (checked) {
+                this._faceTracker.add('nose', this.nose);
+                this._guiParameter['face'] = false;
+                this._faceTracker.remove('face');
+            } else {
+                this._faceTracker.remove('nose');
+            }
+        });
+    }
+
+    _setUpMouthEyePartsController(folder) {
+        const mouthController = folder.add(this._guiParameter, 'mouth').name('Mouth');
+        if (mouthController.getValue() === true) {
+            this._faceTracker.add('mouth', this.mouth);
+        }
+        mouthController.listen().onChange((checked) => {
+            if (checked) {
+                this._faceTracker.add('mouth', this.mouth);
+                this._guiParameter['face'] = false;
+                this._faceTracker.remove('face');
+            } else {
+                this._faceTracker.remove('mouth');
+            }
+        });
+    }
+
+    _setUpEyeLineController() {
+        const eyeLine = this._gui.addFolder('Eye Line');
+        eyeLine.open();
+
+        const none = eyeLine.add(this._guiParameter, 'none')
+            .listen()
+            .onChange(() => setEyeLine('none'));
+        const mosaic = eyeLine.add(this._guiParameter, 'mosaic')
+            .listen()
+            .onChange(() => setEyeLine('mosaic'));
+        const line = eyeLine.add(this._guiParameter, 'line')
+            .listen()
+            .onChange(() => setEyeLine('line'));
+
+        const setEyeLine = (prop) => {
+            const eyeLineParameter = [
+                'none',
+                'mosaic',
+                'line'
+            ]
+            for (const param of eyeLineParameter){
+                this._guiParameter[param] = false;
+            }
+            this._guiParameter[prop] = true;
+
+            if (prop !== 'none') {
+                this._faceTracker.addEyeLine(prop);
+                return;
+            }
+            this._faceTracker.addEyeLine('');
+        }
     }
 }
